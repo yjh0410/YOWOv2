5eb793a5e5db238a7290078fbb760b7c8043f17f
yjh0410 <1394571815@qq.com>
2023-02-10 10:33:14 +0800

    update

--- demo.py
+++ demo.py
@@ -196,7 +196,7 @@ def detect(args, model, device, transform, class_names, class_colors):
             out.write(frame_resized)
 
             if args.gif:
-                gif_resized = cv2.resize(frame, (160, 120))
+                gif_resized = cv2.resize(frame, (200, 150))
                 gif_resized_rgb = gif_resized[..., (2, 1, 0)]
                 image_list.append(gif_resized_rgb)
 
