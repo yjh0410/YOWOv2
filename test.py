db1a8c06382534573f462e1ebb98f16e15a52ed1
yjh0410 <1394571815@qq.com>
2023-03-13 10:46:55 +0800

    debug vis

--- test.py
+++ test.py
@@ -179,12 +179,12 @@ def inference_ava(args, model, device, dataset, class_names=None, class_colors=N
                 # scores, indices  = [list(a) for a in zip(*sorted(zip(scores,indices), reverse=True))] # if you want, you can sort according to confidence level
                 for _, cls_ind in enumerate(indices):
                     text.append("[{:.2f}] ".format(scores[_]) + str(class_names[cls_ind]))
-                    text_size.append(cv2.getTextSize(text[-1], font, fontScale=0.3, thickness=1)[0])
+                    text_size.append(cv2.getTextSize(text[-1], font, fontScale=0.5, thickness=1)[0])
                     coord.append((x1+3, y1+7+10*_))
                     cv2.rectangle(blk, (coord[-1][0]-1, coord[-1][1]-6), (coord[-1][0]+text_size[-1][0]+1, coord[-1][1]+text_size[-1][1]-4), (0, 255, 0), cv2.FILLED)
-                key_frame = cv2.addWeighted(key_frame, 1.0, blk, 0.25, 1)
+                key_frame = cv2.addWeighted(key_frame, 1.0, blk, 0.5, 1)
                 for t in range(len(text)):
-                    cv2.putText(key_frame, text[t], coord[t], font, 0.25, (0, 0, 0), 1)
+                    cv2.putText(key_frame, text[t], coord[t], font, 0.5, (0, 0, 0), 1)
         
         if args.show:
             cv2.imshow('key-frame detection', key_frame)
